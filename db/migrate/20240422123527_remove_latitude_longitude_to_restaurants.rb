fdfc7b1d93a81fb7ac8f08ca1c67134bc989ea39
sema karan <karan18sema@gmail.com>
2024-04-22 23:20:25 +0200

    migration update

--- db/migrate/20240422123527_remove_latitude_longitude_to_restaurants.rb
+++
@@ -1,6 +0,0 @@
-class RemoveLatitudeLongitudeToRestaurants < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :restaurants, :latitude, :float
-    remove_column :restaurants, :longtitude, :float
-  end
-end
